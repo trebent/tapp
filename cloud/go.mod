1c5b315cdd2466f61d89d0045c7967fc99ae9092
Måns Thörnvik <thornvik.mans@gmail.com>
2025-08-23 15:07:45 +0200

    add cloud server foundation

---
+++ cloud/go.mod
@@ -0,0 +1,16 @@
+module github.com/trebent/tapp-backend
+
+go 1.24.3
+
+require (
+	github.com/trebent/envparser v1.0.5
+	github.com/trebent/zerologr v1.0.1
+)
+
+require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ cloud/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/trebent/envparser"
+	"github.com/trebent/zerologr"
+)
+
+var (
+	addr = envparser.Register(&envparser.Opts[string]{
+		Value: ":8080",
+		Name:  "ADDR",
+		Desc:  "Address to listen on",
+	})
+	logToConsole = envparser.Register(&envparser.Opts[bool]{
+		Value: true,
+		Name:  "LOG_TO_CONSOLE",
+		Desc:  "Log to console instead of JSON",
+	})
+	logLevel = envparser.Register(&envparser.Opts[int]{
+		Value: 0,
+		Name:  "LOG_LEVEL",
+		Desc:  "Log level (higher is more verbose), negative values are invalid",
+		Validate: func(v int) error {
+			if v < 0 {
+				return fmt.Errorf("value is negative: %d", v)
+			}
+			return nil
+		},
+	})
+)
+
+func main() {
+	flag.Usage = func() {
+		_, _ = fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		_, _ = fmt.Fprintf(flag.CommandLine.Output(), "%s:\n", envparser.Help())
+		flag.PrintDefaults()
+	}
+
+	// ExitOnError is set to true, so this will exit on error.
+	_ = envparser.Parse()
+
+	zerologr.Set(zerologr.New(&zerologr.Opts{
+		Console: logToConsole.Value(),
+		Caller:  true,
+		V:       logLevel.Value(),
+	}))
+
+	signalCtx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	httpServer := &http.Server{
+		Addr:         addr.Value(),
+		Handler:      http.DefaultServeMux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		if err := httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) && err != nil {
+			zerologr.Error(err, "server start failed")
+			os.Exit(1)
+		}
+		zerologr.Info("server stopped gracefully")
+	}()
+
+	<-signalCtx.Done()
+	zerologr.Info("server shutting down")
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+	if err := httpServer.Shutdown(shutdownCtx); err != nil {
+		zerologr.Error(err, "server shutdown failed")
+		os.Exit(1)
+	}
+}
