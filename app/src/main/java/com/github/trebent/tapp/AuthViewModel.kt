93af04104f02dda5a97694e84f2cf52f0fdbf78e
Måns Thörnvik <thornvik.mans@gmail.com>
2025-08-23 13:18:00 +0200

    correct navigation on boot to either home or login

--- app/src/main/java/com/github/trebent/tapp/AuthViewModel.kt
+++ app/src/main/java/com/github/trebent/tapp/AuthViewModel.kt
@@ -1,32 +1,52 @@
 package com.github.trebent.tapp
 
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 
 const val testUsername = "u"
 const val testPassword = "p"
 
 
-class AuthViewModel(var loginState: Boolean = false) : ViewModel() {
+class AuthViewModel() : ViewModel() {
+    private val _loginState = MutableStateFlow(false)
+
+    // Used for relaying to the MainActivity if the splash screen can be removed or not.
+    private val _i = MutableStateFlow(false)
+    val initialised = _i.asStateFlow()
+
+    init {
+        Log.i("AuthViewModel", "initialising the auth view model")
+        // TODO: remove hardcoded sleep
+        Thread.sleep(200)
+        // TODO: add cloud login check
+        _i.value = true
+    }
 
     fun isLoggedIn(): Boolean {
-        return loginState
+        Log.i("AuthViewModel", "isLoggedIn ${_loginState.value}")
+        return _loginState.value
     }
 
     fun signup(tag: String, email: String, password: String): Boolean {
+        Log.i("AuthViewModel", "signup")
         return true
     }
 
     fun login(email: String, password: String): Boolean {
         if (BuildConfig.TAPP_TEST_MODE && email == testUsername && password == testPassword) {
-            loginState = true
+            Log.i("AuthViewModel", "login succeeded")
+            _loginState.value = true
         }
 
-        return loginState
+        return _loginState.value
     }
 
     fun logout() {
-        loginState = false
+        Log.i("AuthViewModel", "logout")
+        _loginState.value = false
     }
 }
--- app/src/main/java/com/github/trebent/tapp/MainActivity.kt
+++ app/src/main/java/com/github/trebent/tapp/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.github.trebent.tapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -21,51 +25,76 @@ import com.github.trebent.tapp.ui.theme.TappTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val authViewModel: AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Keep the splash as long as the auth view model needs to do its cloud sync.
         val splash = installSplashScreen()
-        var i: Int = 0
         splash.setKeepOnScreenCondition {
-            i++
-            i != 20
+            !authViewModel.initialised.value
         }
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContent {
-            TappTheme { Main() }
+            TappTheme { Main(authViewModel) }
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Main(authViewModel: AuthViewModel = viewModel()) {
-    val startDestination = if (authViewModel.isLoggedIn()) "home" else "login"
+fun Main(authViewModel: AuthViewModel) {
+    Log.i("Main", "main compose entrypoint")
     val navController = rememberNavController()
+
+    val isLoggedIn = authViewModel.isLoggedIn()
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
     ) { padding ->
         NavHost(
             navController = navController,
-            startDestination = startDestination,
+            startDestination = if (isLoggedIn) "home" else "login",
             modifier = Modifier
                 .padding(padding)
                 .padding(horizontal = 16.dp)
         ) {
+            composable("splash") { SplashScreen() }
             composable("home") { HomeScreenRoute(authViewModel) { navController.navigate("login") } }
             composable("login") {
                 LoginScreenRoute(
                     authViewModel,
-                    { navController.navigate("signup") }) { navController.navigate("home") }
+                    { navController.navigate("signup") }
+                ) { navController.navigate("home") }
             }
             composable("signup") {
                 SignupScreenRoute(
                     authViewModel,
-                    { navController.navigate("login") }) { navController.navigate("login") }
+                    { navController.navigate("login") }
+                ) { navController.navigate("login") }
             }
+        }
+    }
+
+    // One-off navigation post NavHost graph-setting and view model initialisation.
+    LaunchedEffect(isLoggedIn) {
+        if (isLoggedIn) {
+            navController.navigate("home")
+        } else {
+            navController.navigate("login")
         }
     }
 }
 
+@Composable
+fun SplashScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {}
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
