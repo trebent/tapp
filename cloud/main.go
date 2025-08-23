372f3c8688ce3c72ae0e9b43ed2521ab5099c415
Måns Thörnvik <thornvik.mans@gmail.com>
2025-08-23 15:23:45 +0200

    fix linting issues

--- cloud/main.go
+++ cloud/main.go
@@ -16,16 +16,19 @@ import (
 )
 
 var (
+	//nolint:gochecknoglobals // Global vars are fine for env vars.
 	addr = envparser.Register(&envparser.Opts[string]{
 		Value: ":8080",
 		Name:  "ADDR",
 		Desc:  "Address to listen on",
 	})
+	//nolint:gochecknoglobals // Global vars are fine for env vars.
 	logToConsole = envparser.Register(&envparser.Opts[bool]{
 		Value: true,
 		Name:  "LOG_TO_CONSOLE",
 		Desc:  "Log to console instead of JSON",
 	})
+	//nolint:gochecknoglobals // Global vars are fine for env vars.
 	logLevel = envparser.Register(&envparser.Opts[int]{
 		Value: 0,
 		Name:  "LOG_LEVEL",
@@ -39,7 +42,14 @@ var (
 	})
 )
 
+const (
+	readTimeout     = 5 * time.Second
+	writeTimeout    = 5 * time.Second
+	shutdownTimeout = 5 * time.Second
+)
+
 func main() {
+	//nolint:reassign // This is intended
 	flag.Usage = func() {
 		_, _ = fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
 		_, _ = fmt.Fprintf(flag.CommandLine.Output(), "%s:\n", envparser.Help())
@@ -61,13 +71,14 @@ func main() {
 	httpServer := &http.Server{
 		Addr:         addr.Value(),
 		Handler:      http.DefaultServeMux,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
 	}
 
 	go func() {
 		if err := httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) && err != nil {
 			zerologr.Error(err, "server start failed")
+			//nolint:gocritic // I know.
 			os.Exit(1)
 		}
 		zerologr.Info("server stopped gracefully")
@@ -75,7 +86,7 @@ func main() {
 
 	<-signalCtx.Done()
 	zerologr.Info("server shutting down")
-	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), shutdownTimeout)
 	defer shutdownCancel()
 	if err := httpServer.Shutdown(shutdownCtx); err != nil {
 		zerologr.Error(err, "server shutdown failed")
